f4260a1dc0ce03377107727ed841b57cc72aa67e
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-09-17 10:01:54 -0600

    Add status

--- app.js
+++ app.js
@@ -8,30 +8,30 @@ const host = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (request, response, next) => {
+app.get("/", (request, response) => {
   queries.allStudents()
     .then(result => response.json({students: result}))
 });
 
-app.get("/:id", (request, response, next) => {
+app.get("/:id", (request, response) => {
   queries.getStudent(request.params.id)
     .then(result => response.json({student: result}))
 });
 
-app.post("/", (request, response, next) => {
+app.post("/", (request, response) => {
   queries.createStudent(request.body)
-    .then(newStudentData => response.json({newStudent: newStudentData}));
+    .then(newStudentData => response.status(201).json({newStudent: newStudentData}));
 });
 
-app.put("/:id", (request, response, next) => {
+app.put("/:id", (request, response) => {
   queries.updateStudent(request.params.id, request.body)
-    .then(result => response.json({updatedStudent: result}))
+    .then(result => response.status(200).json({updatedStudent: result}))
 
 })
 
-app.delete("/:id", (request, response, next) => {
+app.delete("/:id", (request, response) => {
   queries.deleting(request.params.id)
-    .then(result => response.json({result}))
+    .then(result => response.status(204).json({result}))
 });
 
 app.listen(host, () => (console.log(`Listening on PORT ${host}`)));
